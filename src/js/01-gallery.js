91d4ec5eb450d0701a814e32c29b58ee1bb02e80
Alina Dogaru <alina.elena_dogaru@yahoo.com>
2024-08-21 00:34:18 +0300

    Update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+// Descris în documentație
+import SimpleLightbox from "simplelightbox";
+// Import suplimentar de stil
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallerySelector = document.querySelector(`.gallery`);
+ 
+
+const markUpGallerySecond = (items) => {
+    return items
+    .map((item) => {
+        const { preview, original, description } = item;
+        return `<li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" data-source="${original}"
+                alt="${description}"/>
+                </a>
+               </li>`;
+        }).join('');
+};
+
+console.log(markUpGallerySecond(galleryItems));
+
+const createInnerHtml = () => {
+    gallerySelector.innerHTML = markUpGallerySecond(galleryItems);
+}
+createInnerHtml();
+
+function preventDefault(event) {
+    event.preventDefault();
+}
+gallerySelector.addEventListener("click", preventDefault)
+let library = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+"use strict"
 
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+
+const player = new Vimeo('vimeo-player');
+
+const throttleSavePlaybackTime = throttle(savePlaybackTime, 1000);
+
+function savePlaybackTime(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+
+player.on('timeupdate', (data) => {
+  throttleSavePlaybackTime(data.seconds);
+});
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime).then(() => {
+    player.play();
+  });
+};
