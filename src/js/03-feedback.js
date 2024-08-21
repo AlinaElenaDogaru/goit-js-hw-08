a8dcf76c68edd1a95a9353e8bc6be0df8da1971a
Alina Dogaru <alina.elena_dogaru@yahoo.com>
2024-08-21 09:11:26 +0300

    Update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+"use strict";
+import throttle from "lodash.throttle";
+
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message');
+
+// Funcție pentru a salva starea formularului în local storage
+const save = () => {
+     const feedbackFormState = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState))
+};
+
+form.addEventListener("input", throttle(save, 500));
+
+// Funcție pentru a încărca starea formularului din local storage
+
+const loadFunction = () => {
+
+   const savedContent = localStorage.getItem("save");
+   if (savedContent) {
+       const parsedState = JSON.parse(savedState);
+       emailInput.value = parsedState.email;
+        messageInput.value = parsedState.message;
+    }
+}
+       
+window.addEventListener("load", loadFunction);
+
+// Funcție pentru a afișa obiectul cu câmpurile email și message în consolă
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const feedbackFormState = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    console.log(feedbackFormState);
+    localStorage.removeItem("feedback-form-state");
+}
+
+form.addEventListener("submit", handleSubmit);
 
